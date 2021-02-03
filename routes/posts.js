bd76113e79633d6aa2bb2e2bb0b33a44ba50701b
Developer genius <developer_genius@protonmail.com>
2021-02-03 16:39:47 -0500

    update controller updated routes

---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+const { Router } = required('express');
+const controller = require('../controller/posts');
+
+
+const router = Router();
+
+router.get('/posts', controller.getPosts);
+router.get('/posts/:id', controller.getPost);
+router.post('/posts', controller.createPost);
+router.put('/posts/:id', controller.updatePost);
+router.delete('/posts/:id', controller.deletePost);
+
+
+module.exports = router;
