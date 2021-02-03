d6655b4a68f8b13c6fe8056111ab89a975fdd60a
Daniel Ramirez <danielramirez@daniels-mbp.home>
2021-02-03 15:23:48 -0500

    -updated files: -App -Nav jsx,css -index js -blogs jsx -home jsx

--- client/src/screens/Blogs/Blogs.jsx
+++ client/src/screens/Blogs/Blogs.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Blogs.css";
+import Layout from "../../components/shared/Layout/Layout";
 
 const Blogs = () => {
   return (
-    <div>
-      <h1>Blogs</h1>
-    </div>
+    <Layout>
+      <div>
+        <h1>Blogs</h1>
+      </div>
+    </Layout>
   );
 };
 
