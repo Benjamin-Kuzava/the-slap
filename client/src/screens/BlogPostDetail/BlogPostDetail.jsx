913b4d41cab29f9e1fff8f72f56298ada7f9e9ec
benjamin kuzava <benjamin@kuzava.local>
2021-02-05 13:07:45 -0500

    Fixed routing issue in services/posts.js Small css fix to detail page Connected detail page with API. Grabs correct post when clicked on Home page. Delete request working, but not routing back to home page after the fact Have not added edit request or route yet

--- client/src/screens/BlogPostDetail/BlogPostDetail.jsx
+++ client/src/screens/BlogPostDetail/BlogPostDetail.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../components/shared/Layout/Layout";
 import "./BlogPostDetail.css";
+import { getPost, deletePost } from "../../services/posts";
+import { useParams } from "react-router-dom";
 
-const BlogPostDetail = () => {
+const BlogPostDetail = (props) => {
+  const [post, setPost] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const post = await getPost(id);
+      setPost(post);
+      setIsLoaded(true);
+    };
+
+    fetchPost();
+  }, [id]);
+
+  if (!isLoaded) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <Layout>
-      <div>
-        <h1>BlogPostDetail</h1>
+      <div className="blog-detail">
+        <h1>{post.title}</h1>
+        <img src={post.imgURL}></img>
+        <h2>{post.description}</h2>
+        <h3>{post.author}</h3>
+        <div className="buttons">
+          <button>Edit</button>
+          <button onClick={() => deletePost(post._id)}>Delete</button>
+        </div>
       </div>
     </Layout>
   );
